9a85948de51a69a100304dacabb47c72b9ea630e
LuMe <going3489.dev.qm7eo@slmail.me>
2024-06-25 11:42:33 +0200

    streamlit update

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -1,19 +1,46 @@
+from importlib import reload
+
+from pages import intro, data_analysis, machine_learning, lenet, transfer_learning, interpretability, model, summary, team
 import streamlit as st
-# from models.predict_model import predict_image
-from PIL import Image
-import os
 
-st.title('Plant Disease Detection')
+reload(intro)
+reload(data_analysis)
+reload(machine_learning)
+reload(lenet)
+reload(transfer_learning)
+reload(interpretability)
+reload(model)
+reload(summary)
+reload(team)
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.write("Classifying...")
-    # Save the uploaded image
-    file_path = os.path.join('/path/to/the/uploads', uploaded_file.name)
-    image.save(file_path)
-    # Prediction
-    # prediction = predict_image(file_path)
-    # st.write(f'Prediction: {prediction}')
+
+PAGES = {
+    "Intro": intro,
+    "Data Analysis": data_analysis,
+    "Machine Learning": machine_learning,
+    "LeNet": lenet,
+    "Transfer Learning": transfer_learning,
+    "Interpretability": interpretability,
+    "Prediction": model,
+    "Summary": summary,
+    "Team": team,
+}
+
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="Leukopy", page_icon="🩸")
+
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+    page = PAGES[selection]
+    page.write()
+    # with st.spinner(f"Loading {selection} ..."):
+    #     ast.shared.components.write_page(page)
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        This app is maintained by the leukopy team. You can learn more about me at
+        [leukopy](https://github.com/DataScientest/Leukopy).
+        """
+    )
